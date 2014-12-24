ee3e477b03c014c11500c05300ade3433a77d1a6
aaron levin <vilevin@gmail.com>
2014-12-23 22:50:49 -0500

    Add nix support

---
+++ default.nix
@@ -0,0 +1,34 @@
+{ nixpkgs ? (import <nixpkgs> {})
+, haskell ? (import <nixpkgs>{}).haskellPackages }:
+let
+in rec {
+  haskellShopifyEnv = nixpkgs.myEnvFun {
+    name = "haskell-shopify";
+
+    buildInputs = [
+      # build depends
+      haskell.aeson
+      haskell.comonad
+      haskell.conduit
+      haskell.free
+      haskell.httpConduit
+      haskell.mtl
+      haskell.resourcet
+      haskell.scientific
+      haskell.text
+      haskell.time
+      haskell.unorderedContainers
+
+      # test depends
+      haskell.HUnit
+      haskell.QuickCheck
+      haskell.rawStringsQq
+      haskell.testFramework
+      haskell.testFrameworkHunit
+      haskell.testFrameworkQuickcheck2
+
+      # editor tooling
+      haskell.ghcMod
+    ];
+  };
+}
